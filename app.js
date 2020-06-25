720abc21acd50565271bccc880e97d45b58ffc3e
krushna_panchal <krushna.panchal@systemc.com>
2020-06-25 14:47:44 +0000

    Fixed the sort function, now the colors are bound to the number cells.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 
-var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numArray = [[1, 'color-one'], [2, 'color-two'], [3, 'color-three'], [4, 'color-two'], [5, 'color-three'], [6, 'color-four'], [7, 'color-four'], [8, 'color-one'], [9, 'color-three']];
 
 function shuffle() {
     var allGridItems = document.querySelectorAll('.grid-item');
-
     var currentIndex = numArray.length, temporaryValue, randomIndex;
 
     while (0 !== currentIndex) {
@@ -11,9 +10,11 @@ function shuffle() {
         currentIndex -= 1;
         temporaryValue = numArray[currentIndex];
         numArray[currentIndex] = numArray[randomIndex];
-        allGridItems[currentIndex].innerHTML = numArray[randomIndex];
+        allGridItems[currentIndex].innerHTML = numArray[randomIndex][0];
+        allGridItems[currentIndex].className = 'grid-item ' + numArray[randomIndex][1];
         numArray[randomIndex] = temporaryValue;
-        allGridItems[randomIndex].innerHTML = temporaryValue;
+        allGridItems[randomIndex].innerHTML = temporaryValue[0];
+        allGridItems[randomIndex].className = 'grid-item ' + temporaryValue[1];
     }
 }
 
@@ -21,6 +22,7 @@ function sort() {
     this.numArray.sort();
     var allGridItems = document.querySelectorAll('.grid-item');
     for (var i = 0; i < allGridItems.length; i++) {
-        allGridItems[i].innerHTML = this.numArray[i];
+        allGridItems[i].innerHTML = this.numArray[i][0];
+        allGridItems[i].className = 'grid-item ' + numArray[i][1];
     }
 }
